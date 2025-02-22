de0cfb32c28c5d44a5dc3b39c3b69e1af543ee7e
Jackkade <mmklop.mmklop@gmail.com>
2025-02-22 12:19:40 -0600

    Constructor for Location

--- Location.h
+++ Location.h
@@ -4,10 +4,18 @@
 
 class Location {
 public:
+
+    Location(Climate c, Terrain t, Vegetation v) : m_climate(c), m_terrain(t), m_vegetation(v) {}
+
+    Climate getClimate() { return m_climate; }
+    Terrain getTerrain() { return m_terrain; }
+    Vegetation getVegetation() { return m_vegetation; }
+
     void changeTerrain(Terrain t);
     void changeVegetation(Vegetation v);
     void changeClimate(Climate c);
 private:
+    //Need to add names
     Climate m_climate;
     Terrain m_terrain;
     Vegetation m_vegetation;
